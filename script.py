af4d657cf66310b12a84dd3cf291e174bea3fd0e
Charles Weems <charlie.weems@gmail.com>
2016-09-14 10:39:39 -0400

    Final

--- script.py
+++ script.py
@@ -1,10 +1,27 @@
-from xml.etree import ElementTree as et
+from lxml import etree as et
 import random
 
-doc = et.parse('500RandomResponses.xml')
+parser = et.XMLParser(remove_blank_text=True)
+doc = et.parse('500RandomResponses.xml', parser)
 
 for partyId in doc.iter('partyId'):
-    partyId.text = random.randint(1,11)*5
+    partyId.text = str(random.randint(100000000000000000,999999999999999999))
     print partyId.text
 
-doc.write('result.xml')
+for addressId in doc.iter('addressId'):
+    addressId.text = str(random.randint(100000000000000000,999999999999999999))
+
+for searchResult in doc.iter('searchResult'):
+    gender = ''
+    random_number = random.randint(1,3)
+    if random_number == 1:
+        gender = 'F'
+    if random_number == 2:
+        gender = 'O'
+    if random_number == 3:
+        gender = 'M'
+
+    genderType = et.SubElement(searchResult, 'genderType')
+    genderType.set('code', gender)
+
+doc.write('result.xml', pretty_print=True)
